89a85daf0419d8ae40d96a32b2250b69a884ceca
Aman <aman.saamaibekov@gmail.com>
2023-11-12 19:32:44 +0600

    react native structure

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import { View, ScrollView, SafeAreaView } from "react-native"
+
+const Home = () => {
+  return (
+    <View>
+    
+    </View>
+  )
+}
+
+export default Home
